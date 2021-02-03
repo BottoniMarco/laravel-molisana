557c9933678ee7546deaf761e7c715939b90ca61
marco bottoni <bottoni.mrc@gmail.com>
2021-02-03 16:01:54 +0100

    molisana 1.0

---
+++ resources/views/home.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href= "css/app.css" >
+        
+
+        <!-- Styles -->
+     </head>
+    <body>
+        <header>
+            <img src="{{ asset('images/marchio-sito-test.png') }}" alt="">
+            <ul class="list-inline">
+                <li class="active">
+                    <a href="#" >HOME</a>
+                </li>
+
+                <li>
+                    <a href="#">PRODOTTI</a>
+                </li>
+
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+
+
+            </ul>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
